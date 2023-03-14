745a57ee762bfbfdb84ac36926d61a0695d2192a
Sung Hoon KIM <92258672+newcave@users.noreply.github.com>
2023-03-14 15:53:49 +0900

    Update app.py

--- app.py
+++ app.py
@@ -45,8 +45,8 @@ def app():
     sidebar = st.sidebar
     time_end = sidebar.slider("Time end (hrs.)", min_value=1, max_value=28, value=10, step=1)
     dt = sidebar.slider("Time step (hrs.)", min_value=0.1, max_value=1.0, value=0.2, step=0.1)
-    x_0 = sidebar.slider("Initial Streamflow estimate", min_value=0.0, max_value=75.0, value=12.0, step=0.1)
-    P_0 = sidebar.slider("Initial error covariance estimate", min_value=0.0, max_value=90.0, value=6.0, step=0.1)
+    x_0 = sidebar.slider("Initial Streamflow estimate", min_value=0.0, max_value=200.0, value=75.0, step=10)
+    P_0 = sidebar.slider("Initial error covariance estimate", min_value=0.0, max_value=200.0, value=90.0, step=0.5)
 
     time = np.arange(0, time_end, dt)
     n_samples = len(time)
